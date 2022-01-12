f239d65a0f3f3aad4c01af6eb9276e90f0171551
Miffop <sash.kryuko2015@gmail.com>
2022-01-12 17:47:28 +0300

    MemoryImplemented

--- Code.js
+++ Code.js
@@ -28,3 +28,32 @@ DelClick.onclick=()=>{
         scr.innerText=scr.innerText.slice(0,scr.innerText.length-1)
     }
 }
+let Mem=document.getElementById("Memory");
+let MemClick=document.getElementById("memClick");
+
+MemClick.onclick=()=>{
+    if(scr.innerText.length>0){
+        let Lidx=document.createElement("li");
+
+        let p=document.createElement("span");
+        p.appendChild(document.createTextNode(scr.innerText));
+        p.style.width="400px";
+        p.onclick=()=>{
+            scr.innerText+=p.innerText;
+        }
+
+        let img=document.createElement("img");
+        img.src="remove.png";
+        img.style.width="20px"
+        img.style.marginLeft="20px"
+        img.onclick=()=>{
+            Mem.removeChild(Lidx);
+        }
+
+        Lidx.appendChild(p);
+        Lidx.appendChild(img);
+        Mem.appendChild(Lidx);
+    }
+}
+
+
